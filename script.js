d6bf7e7b1831b502a330592ffaae0f9d1dd1d1e9
ajaidanial <ajaidanial@gmail.com>
2019-08-30 14:42:19 +0530

    bug solved

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const HOURS = 1 // default: 24
-const MINUTES = 0.1 // default: 60
-var target_date, days, hours, minutes, seconds, countdown;
+const MINUTES = 0.3 // default: 60
+const countdown = document.getElementById("tiles"); // get tag element 
+var target_date, days, hours, minutes, seconds;
 
 window.onload = function () {
 	if (localStorage.getItem("timer") === null) {
 		target_date = new Date().getTime() + (1000 * MINUTES * 60 * HOURS); // initialize the time
 	} else {
-		onStartButtonClick()
 		target_date = localStorage.getItem("timer") // retrieve the saved time
+		document.getElementById("startButton").style.display = 'none';
+		getCountdown()
+		var timeOutFucntion = setInterval(function () {
+			if (((target_date - new Date().getTime()) / 1000) <= 0) {
+				document.getElementById("countdown").style.display = 'none';
+				document.getElementById("stopHacking").style.display = 'block';
+				localStorage.removeItem("timer")
+				clearTimeout(timeOutFucntion)
+			} else {
+				getCountdown();
+			}
+		}, 1000);
 	}
-	days, hours, minutes, seconds; // variables for time units
-	countdown = document.getElementById("tiles"); // get tag element
-	getCountdownInit();
+	getCountdown();
 }
 
 // start the timer manually
 function onStartButtonClick() {
+	target_date = new Date().getTime() + (1000 * MINUTES * 60 * HOURS); // initialize the time
+	localStorage.setItem("timer", target_date) // save the time here
 	document.getElementById("startButton").style.display = 'none';
+	getCountdown()
 	var timeOutFucntion = setInterval(function () {
 		if (((target_date - new Date().getTime()) / 1000) <= 0) {
 			document.getElementById("countdown").style.display = 'none';
@@ -28,8 +41,7 @@ function onStartButtonClick() {
 	}, 1000);
 }
 
-// displays the initial values | called only once
-function getCountdownInit() {
+function getCountdown() {
 	// find the amount of "seconds" between now and target
 	var current_date = new Date().getTime();
 	var seconds_left = (target_date - current_date) / 1000;
@@ -45,22 +57,6 @@ function getCountdownInit() {
 	} else {
 		countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
 	}
-}
-
-//  updates the timer | called using recursion
-function getCountdown() {
-	localStorage.setItem("timer", target_date) // save the time here
-	// find the amount of "seconds" between now and target
-	var current_date = new Date().getTime();
-	var seconds_left = (target_date - current_date) / 1000;
-	days = pad(parseInt(seconds_left / 86400));
-	seconds_left = seconds_left % 86400;
-	hours = pad(parseInt(seconds_left / 3600));
-	seconds_left = seconds_left % 3600;
-	minutes = pad(parseInt(seconds_left / 60));
-	seconds = pad(parseInt(seconds_left % 60));
-	// format countdown string + set tag value
-	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
 }
 
 // helper function for displaying
